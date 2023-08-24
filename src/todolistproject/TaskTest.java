880a5352f6b9d07264c39715af721c6c0fa469de
Alyona Dyak <miliona19@gmail.com>
2023-08-24 17:29:29 +0200

    feat: create ToDoListTest Class

--- src/todolistproject/TaskTest.java
+++ src/todolistproject/TaskTest.java
@@ -1,4 +1,23 @@
 package todolistproject;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskTest {
+    @Test
+    public short testTaskDescription() {
+        Task task = new Task("Sample Task");
+        assertEquals("Sample Task", testTaskDescription());
+        return 0;
+    }
+
+    @Test
+    public void testTaskIsDone() {
+        Task task = new Task("Sample Task");
+        assertFalse(task.isDone());
+        task.markAsDone();
+        assertTrue(task.isDone());
 }
+}
+
