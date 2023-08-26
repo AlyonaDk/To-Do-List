50995f6b6ba257e5370ce3d869d561a0e92a59e6
Alyona Dyak <miliona19@gmail.com>
2023-08-26 16:10:41 +0200

    Add: Task and ToDoList classes

---
+++ src/todolistproject/Task.java
@@ -0,0 +1,24 @@
+package todolistproject;
+public class Task {
+    private String description;
+    private boolean isDone;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public boolean isDone() {
+        return isDone;
+    }
+    public void markAsDone() {
+        isDone = true;
+    }
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+}
+
+
--- src/todolistproject/ToDoList.java
+++ src/todolistproject/ToDoList.java
@@ -1,4 +1,13 @@
 package todolistproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToDoList {
+    private List<Task> tasks;
+
+    public ToDoList() {
+        tasks = new ArrayList<>();
+    }
 }
+
